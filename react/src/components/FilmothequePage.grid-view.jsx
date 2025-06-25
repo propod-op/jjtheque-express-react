43b319940ee438b866394175db61a2b442f89cba
propod-op <o.pochic@gmail.com>
2025-06-25 19:28:27 +0200

    Ok GRID is clean and works fine + mobile/PC

--- react/src/components/FilmothequePage.grid-view.jsx
+++ react/src/components/FilmothequePage.grid-view.jsx
@@ -25,68 +25,77 @@ const GridViewFilms = () => {
   if (loading) return <p>Chargement des films...</p>;
   if (erreur) return <p>Erreur : {erreur}</p>;
   // Fonction pour masquer le guest (fermeture)
-  
+
   const showGuest = () => {
     setFilmSelectionne(null);
   };
 
-
   return (
     <>
-      <div className="close-guest" onClick={showGuest}>X</div>
+      <div className="close-guest" onClick={showGuest}>
+        X
+      </div>
       <div className="grid-title">LISTE DES FILMS 2023</div>
-      <div className="guest-zone" style={{ display: filmSelectionne ? "flex" : "none" }}>
+      <div
+        className="guest-zone"
+        style={{ display: filmSelectionne ? "flex" : "none" }}
+      >
         {filmSelectionne ? (
-          
           <div className="guest-card">
-            
-            <div className="block-1">
-              <div className="title-container">
-                <h2>{filmSelectionne.name}</h2>
-                <p>Note : {filmSelectionne.vote_average}</p></div>
-              {filmSelectionne?.overview && (
-                <p className="overview">{filmSelectionne.overview.slice(0,600)}  {filmSelectionne.overview.length > 300 && '...'}</p>
-              )}
-              {filmSelectionne?.actors && (
-                <p className="actors">{filmSelectionne.actors.join(', ')}</p>
-              )}
+            <div className="block-content">
+              <div className="content-container">
+                <div className="title-container">
+                  <h2>{filmSelectionne.name}</h2>
+
+                  <p>Note : {filmSelectionne.vote_average}</p>
+                </div>
+                <p className="overview-title">Résumé</p>
+                {filmSelectionne?.overview && (
+                  <p className="overview-content">
+                    {filmSelectionne.overview.slice(0, 600)}{" "}
+                    {filmSelectionne.overview.length > 300 && "..."}
+                  </p>
+                )}
+                <p className="actors-title">Acteurs</p>
+                {filmSelectionne?.actors && (
+                  <p className="actors-content">{filmSelectionne.actors.join(", ")}</p>
+                )}
+              </div>
             </div>
 
-            <div className="block-2">
+            <div className="block-image">
               <img
                 src={`https://image.tmdb.org/t/p/w300${filmSelectionne.poster_path}`}
                 alt={filmSelectionne.title}
               />
             </div>
-
           </div>
-
         ) : (
           <p>GUEST A AFFICHER SI AUCUN SELECTIONNE</p>
         )}
       </div>
-    <section className="films">
-      <div className="grid-zone">
-        {films.map((film, index) => (
-          <button
-            className="grid-item"
-            key={film.id || `film-${index}`}
-            onClick={() => setFilmSelectionne(film)}
-            aria-label={`Sélectionner ${film.name}`}
-          >
-            <h2>{film.name}</h2>
-            {film.poster_path ? (
-              <img
-                src={`https://image.tmdb.org/t/p/w200${film.poster_path}`}
-                alt={film.title}
-              />
-            ) : (
-              <p>Aucune image disponible</p>
-            )}
-          </button>
-        ))}
-      </div>
-    </section>
+      <section className="films">
+        <div className="grid-zone">
+          {films.map((film, index) => (
+            <button
+              className="grid-item"
+              key={film.id || `film-${index}`}
+              onClick={() => setFilmSelectionne(film)}
+              aria-label={`Sélectionner ${film.name}`}
+            >
+              <h2>{film.name}</h2>
+              {film.poster_path ? (
+                <img
+                  src={`https://image.tmdb.org/t/p/w200${film.poster_path}`}
+                  alt={film.title}
+                />
+              ) : (
+                <p>Aucune image disponible</p>
+              )}
+            </button>
+          ))}
+        </div>
+      </section>
     </>
   );
 };
